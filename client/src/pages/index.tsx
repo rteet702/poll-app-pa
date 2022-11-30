50d7b10d70a1fef0ec580369b16d9dc8a2070e94
Robert Teets <teetsrobert@gmail.com>
2022-11-29 18:29:23 -0700

    feat: linked client and server together

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,25 @@
-import { NextPage } from "next";
+import axios from "axios";
+import { GetServerSideProps, NextPage } from "next";
 
-const Home: NextPage = () => {
+type Props = {
+    response: string;
+};
+
+const Home: NextPage<Props> = ({ response }) => {
     return (
         <div>
-            <h1 className="text-4xl">Hello World!</h1>
+            <h1 className="text-4xl">{response}</h1>
         </div>
     );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const response = await axios.get(process.env.SERVER_URL + "/api/test");
+    return {
+        props: {
+            response: response.data.message,
+        },
+    };
 };
 
 export default Home;
--- server/routes/generic.routes.ts
+++ server/routes/generic.routes.ts
@@ -3,7 +3,7 @@ import { Express, Request, Response } from "express";
 const router = (app: Express) => {
     app.get("/api/test", (request: Request, response: Response) => {
         response.status(200).json({
-            message: "Test successful!",
+            message: "Yeeeeet successful!",
         });
     });
 };
