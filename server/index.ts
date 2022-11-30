71e08d99f092621b5c4d758a66d2d42ab449cc77
Robert Teets <teetsrobert@gmail.com>
2022-11-29 17:46:25 -0700

    chore: added basic comments for readability

--- server/index.ts
+++ server/index.ts
@@ -2,13 +2,17 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import genericRouter from "./routes/generic.routes";
 
+// dotenv config
 dotenv.config();
 
+// basic declarations
 const app: Express = express();
 const port = process.env.PORT || 8000;
 
+// route declarations
 genericRouter(app);
 
+// spin up server
 app.listen(port, () => {
     console.log(`Server listening on port ${port}!`);
 });
